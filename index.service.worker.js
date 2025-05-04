3c2a4d3f4696e9f175e6f2bcaf27a7d3e500195a
Melondev <KrispeeKreeme@gmail.com>
2025-05-04 10:32:38 +0800

    .

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1746325854|240310805';
+const CACHE_VERSION = '1746325955|341351219';
 /** @type {string} */
 const CACHE_PREFIX = 'keyHunter-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
